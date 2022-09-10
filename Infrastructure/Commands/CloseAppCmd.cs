f783aaff3686317a1591e04d300cb52a2e7171e1
TenNM <pacs3131@gmail.com>
2022-09-10 23:21:41 +0300

    - mvp

---
+++ Infrastructure/Commands/CloseAppCmd.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using WPF_Test.Infrastructure.Commands.Base;
+
+namespace WPF_Test.Infrastructure.Commands
+{
+    internal class CloseAppCmd : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_Test.Infrastructure.Commands;
+using WPF_Test.ViewModels.Base;
+
+namespace WPF_Test.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region title
+
+        private string _title = "Window title";
+
+        /// <summary>Window title</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);//SetProperty
+        }
+        #endregion
+
+        #region commands
+        
+        //public ICommand CloseAppCmd { get; }
+        //private bool CanCloseAppCmdExecute(object p) => true;
+        //private void OnCloseAppCmdExecuted(object p) => Application.Current.Shutdown();
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            //CloseAppCmd = new LambdaCommand(OnCloseAppCmdExecuted, CanCloseAppCmdExecute);
+        }
+    }
+}
